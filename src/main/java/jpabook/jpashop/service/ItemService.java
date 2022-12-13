f93091461e0257e60472be987aa99f6c8ec548c4
yh725 <yh725@kakao.com>
2022-12-13 22:34:50 +0900

    웹 계층 개발 - 상품 수정 - 변경 감지와 병합(merge) - 상품 주문 - 주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,18 @@ public class ItemService {
 	@Transactional
 	public void saveItem(Item item) {
 		itemRepository.save(item);
+	}
+
+	@Transactional
+	public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+		Item findItem = findOne(itemId);
+//		findItem.change(price, name, stockQuantity);
+//		findItem.setPrice(param.getPrice());
+//		findItem.setName(param.getName());
+//		findItem.setStockQuantity(param.getStockQuantity());
+		findItem.setName(name);
+		findItem.setPrice(price);
+		findItem.setStockQuantity(stockQuantity);
 	}
 
 	public List<Item> findItems() {
